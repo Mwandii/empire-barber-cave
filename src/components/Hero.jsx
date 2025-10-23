07bb7b885cd66c5568880ac05887ebae0b166f1b
Athanas Muinde <athanasmwandi5@gmail.com>
2025-10-23 20:23:57 +0300

    style: add style to hero and navbar section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,14 +3,13 @@ import Navbar from "./Navbar";
 function Hero() {
     return (
         <section className="relative h-[400px] bg-cover bg-center"
-        style={{backgroundImage: "url('https://images.unsplash.com/photo-1695173122226-3a932002ab33?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=870')"}}
+        style={{backgroundImage: `linear-gradient(rgba(0,0,0,0.7), rgba(0,0,0,0.7)),
+            url('https://images.unsplash.com/photo-1695173122226-3a932002ab33?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=870')`}}
         >
-            {/* Dark Overlay on hero */}
-            <div className="absolute inset-0 bg-black/50"></div>
             <Navbar/>
-            <div className="absolute inset-0 flex flex-col items-center justify-center text-center text-white px-6">
-                <h2 className="font-bold text-2xl p-2">Where Style Meets Precision</h2>
-                <p className="">Indulge in the art of modern grooming. From precision cuts and flawless fades to revitalizing facials, body scrubs, and massages — every service is crafted to help you look sharp, feel refreshed, and carry yourself with confidence.</p>
+            <div className="absolute inset-0 flex flex-col items-center justify-center text-center text-white font-light px-10 lg:px-25">
+                <h2 className="font-black text-2xl p-2">Where Style Meets Precision</h2>
+                <p className="font-thin">Indulge in the art of modern grooming. From precision cuts and flawless fades to revitalizing facials, body scrubs, and massages — every service is crafted to help you look sharp, feel refreshed, and carry yourself with confidence.</p>
             </div>
         </section>
     )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 function Navbar() {
     return (
-        <nav className="flex justify-between items-center">
-            <h1 className="m-2 p-1 font-bold text-2xl">Empire <span className="text-yellow-500">Barber</span> Cave</h1>
+        <nav className="flex justify-between items-center text-white relative z-10">
+            <h1 className="m-2 p-2 font-bold text-2xl hover:cursor-pointer">Empire <span className="text-yellow-500">Barber</span> Cave</h1>
             <div>
                 <a className="m-2 p-1 font-semibold">About Us</a>
                 <a className="m-2 p-1 font-semibold">Services</a>
