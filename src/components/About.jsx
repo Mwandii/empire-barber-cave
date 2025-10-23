e22bc56c9fcb0af0901e969b15dfe0eabbfad1f4
Athanas Muinde <athanasmwandi5@gmail.com>
2025-10-23 22:28:30 +0300

    feat: build about component

---
+++ src/components/About.jsx
@@ -0,0 +1,23 @@
+function About() {
+  return (
+    <section>
+      <h1 className="font">About Us</h1>
+      <div className="flex">
+        <div>
+          <img src="https://images.unsplash.com/photo-1643252824236-99080a322120?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NjJ8fGJhcmJlciUyMHNob3AlMjBpbWFnZXN8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&q=60&w=500"/>
+        </div>
+        <div>
+          <h2>Redefining Grooming Excellence</h2>
+          <p>
+            More than a cut — it’s a complete grooming experience. From sharp
+            fades to relaxing scrubs, we perfect every detail so you leave
+            looking and feeling your best
+          </p>
+          <button>ABOUT US</button>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default About;
