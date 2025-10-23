0868408c55a89888a25087e3b02ed93699db0d10
Athanas Muinde <athanasmwandi5@gmail.com>
2025-10-23 18:45:10 +0300

    chore: initialized tailwindcss

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+function Navbar() {
+    return (
+        <nav>
+            <h1>Empire Barber Cave</h1>
+            <div>
+                <a>About Us</a>
+                <a>Services</a>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar;
