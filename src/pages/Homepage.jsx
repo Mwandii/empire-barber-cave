d4b9547d08bb1108132ef7bd910fe6e31eb00ef4
Athanas Muinde <athanasmwandi5@gmail.com>
2025-10-25 13:06:12 +0300

    complete services page

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -3,12 +3,12 @@ import Footer from "../components/Footer";
 import Hero from "../components/Hero";
 import Services from "../components/Services";
 import AboutPage from "./AboutPage";
-import ServicesPage from "./ServicesPage";
+import ServicesCatPage from "./ServicesCatPage";
 
 function Homepage() {
     return (
         <>
-        <ServicesPage/>
+        <ServicesCatPage/>
         {/*
         <Hero/>
         <About/>
