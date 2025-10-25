2494873d7718ee439bb0b06461d3a1118b2e5026
Athanas Muinde <athanasmwandi5@gmail.com>
2025-10-25 17:30:52 +0300

    feat: build service data file

---
+++ src/data/ServicesData.js
@@ -0,0 +1,43 @@
+const serviceData = [
+    {
+        id: 1,
+        name: "Hair Services",
+        Services: [
+            { name: "Haircut", price: 200 },
+      { name: "Haircut + Head Massage", price: 300 },
+      { name: "Dye", price: 200 },
+      { name: "Retouch", price: 500 },
+      { name: "Wash & Straighten", price: 150 },
+        ]
+    },
+    {
+    id: 2,
+    name: "Facial & Skin Care",
+    services: [
+      { name: "Face Scrubbing", price: 200 },
+      { name: "Facial", price: 500 },
+      { name: "Full Body Scrub", price: 1500 },
+    ],
+  },
+  {
+    id: 3,
+    name: "Nail Services",
+    services: [
+      { name: "Plain Gel", price: 400 },
+      { name: "Tips + Gel", price: 700 },
+      { name: "Stickons + Gel", price: 600 },
+      { name: "Manicure", price: 500 },
+      { name: "Pedicure", price: 600 },
+      { name: "Nail Polish", price: 200 },
+    ],
+  },
+  {
+    id: 4,
+    name: "Massage & Body Services",
+    services: [
+      { name: "Half Body Massage", price: 700 },
+      { name: "Full Body Massage", price: 2000 },
+      { name: "Back Massage", price: 300 },
+    ],
+  },
+]
